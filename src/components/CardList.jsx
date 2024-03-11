4263e717c09beb3c7a8e42f515509f148e9cc054
YousefMahmod <youssef.shubra@yahoo.com>
2024-03-11 12:50:47 +0200

    Handle colors of cards

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -12,9 +12,13 @@ const CardList = () => {
     "#FF8B64",
     "#55C2E6",
     "#FF5E7D",
+    "#FF8B64",
+    "#55C2E6",
     "#4BCF82",
     "#7335D2",
     "#F1C75B",
+    "#4BCF82",
+    "#7335D2",
   ];
   let colorIndex = 0;
   return (
